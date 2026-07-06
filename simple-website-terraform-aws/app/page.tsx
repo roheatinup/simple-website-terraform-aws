6051b2b5a1f6d6a8b5b98afd3d888a2924493d12
Roaheen Mansuri <roaheenmansuri@gmail.com>
2026-07-06 18:54:44 -0400

    I forgot the directory i was working in

--- simple-website-terraform-aws/app/page.tsx
+++ simple-website-terraform-aws/app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            OH CAPTAIN MY CAPTAIN.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            THE GITHUB ACTIONS CI/CD DEPLOY FLOW WAS TRIGGERED SUCCESSFULLY{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
@@ -29,7 +29,7 @@ export default function Home() {
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Learning
+              YEEEEEBWOI
             </a>{" "}
             center.
           </p>
